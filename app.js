7eb7f7a425b61b28614762a6eaf201623c8e3681
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2022-03-09 21:44:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+// set inital value to zero
+let count = 0;
+// select value and buttons
+const value = document.querySelector( "#value" );
+const btns = document.querySelectorAll( ".btn" );
+
+btns.forEach( function ( btn )
+{
+    btn.addEventListener( 'click', function ( e )
+    {
+        const style = e.currentTarget.classList;
+        if ( style.contains( "decrease" ) )
+        {
+           
+           count--;
+        }
+        else if ( style.contains( "increase" ) )
+        {
+           
+           count++;
+        }
+
+        else  
+        {
+            count = 0;
+        };
+        if ( count > 0 )
+        {
+            value.style.color = "green"
+        }
+        if ( count < 0 )
+        {
+            value.style.color = "red"
+        }
+        if ( count === 0 )
+        {
+            value.style.color = "#222"
+        }
+        value.textContent = count;
+
+    } )
+} )
+
+
+
+
+btns.forEach( function ( btn )
+{
+    btn.addEventListener( 'click', function ( e )
+    {
+        value.style="color: orange;"
+       
+        value.textContent = count;
+
+    } )
+} )
+
+
+
+
+
+
+
+
+
